d5b7f1d39c1a4d77347f40182f9a09142e4cb812
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-02-21 14:10:31 +0300

    ашч

--- src/features/blogs/api/models/input/create-blog.input.model.ts
+++ src/features/blogs/api/models/input/create-blog.input.model.ts
@@ -1,22 +1,22 @@
-import { IsEmpty, IsString, Matches, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 import { Trim } from '../../../../../infrastucture/decorators/transform/trim';
 
 export class BlogsCreateModel {
   @Trim()
   @IsString()
-  @IsEmpty()
+  @IsNotEmpty()
   @MaxLength(15)
   name: string;
 
   @Trim()
   @IsString()
-  @IsEmpty()
+  @IsNotEmpty()
   @MaxLength(500)
   description: string;
 
   @Trim()
   @IsString()
-  @IsEmpty()
+  @IsNotEmpty()
   @MaxLength(100)
   @Matches('^https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$')
   websiteUrl: string;
