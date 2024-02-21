44cf995a9f42d159661c378f6c3e2ce5fc6be206
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-02-21 14:05:14 +0300

    fix: fix blogscreate decorator

--- src/features/blogs/api/models/input/create-blog.input.model.ts
+++ src/features/blogs/api/models/input/create-blog.input.model.ts
@@ -1,19 +1,22 @@
-import { IsString, Matches, MaxLength } from 'class-validator';
+import { IsEmpty, IsString, Matches, MaxLength } from 'class-validator';
 import { Trim } from '../../../../../infrastucture/decorators/transform/trim';
 
 export class BlogsCreateModel {
   @Trim()
   @IsString()
+  @IsEmpty()
   @MaxLength(15)
   name: string;
 
   @Trim()
   @IsString()
+  @IsEmpty()
   @MaxLength(500)
   description: string;
 
   @Trim()
   @IsString()
+  @IsEmpty()
   @MaxLength(100)
   @Matches('^https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$')
   websiteUrl: string;
